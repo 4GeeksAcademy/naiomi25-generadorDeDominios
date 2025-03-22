6a7822eab298aab925f144c7425e0d2452373561
naiomi25 <naiomidiaz.her@gmail.com>
2025-03-22 11:20:14 +0000

    ejercicio de dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const resultado = document.getElementById('js')
+
+    let pronoun = ['mi', 'tu'];
+    let adj = ['rapida', 'genial'];
+    let noun = ['idea', 'web'];
+    let domain = ['io','es']
+    let generadorDeDominios = ''
+
+
+
+    for (let i = 0 ; i < pronoun.length; i++){
+        for (let j = 0 ; j < adj.length; j++){
+            for (let n = 0 ; n < noun.length; n++){
+                for (let d = 0 ; d < domain.length; d++){
+                let dominioFinal = 'www.'+ pronoun[i]+adj[j]+noun[n]+ '.' +domain[d]
+                generadorDeDominios += dominioFinal  + '<br>'
+                }
+            }
+        }
+    }
+    resultado.innerHTML = generadorDeDominios
+    }
+  
