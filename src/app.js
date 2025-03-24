8de28c37a614dab3fd729f1a65d33b4091cc09f0
naiomi25 <naiomidiaz.her@gmail.com>
2025-03-24 20:54:20 +0000

    proyecto modificado

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  const resultado = document.getElementById('js')
+/*window.onload = function() {
+     const resultado = document.getElementById('js')
 
     let pronoun = ['mi', 'tu'];
     let adj = ['rapida', 'genial'];
@@ -28,4 +28,35 @@ window.onload = function() {
     }
     resultado.innerHTML = generadorDeDominios
     }
-  
+  */
+window.onload = function () {
+
+    let pronombres = ['mi', 'tu'];
+    let adjetivos = ['rapida', 'genial'];
+    let nombres = ['idea', 'web'];
+    let dominios = ['io', 'es']
+    let generadorDominios = []
+    let IndiceDominios = 0
+
+
+
+    for (let pronombre of pronombres) {
+        for (let adjetivo of adjetivos) {
+            for (let nombre of nombres) {
+                for (let dominio of dominios) {
+                    let construccionDominio = `www.${pronombre}${adjetivo}${nombre}.${dominio}`
+                    generadorDominios.push(construccionDominio)
+                }
+            }
+        }
+    }
+    let parrafogeneradorDominios = document.getElementById('js')
+    let boton = document.getElementById('botonDominio')
+    function salidaOrdenadaDominios() {
+        if (IndiceDominios < generadorDominios.length)
+            parrafogeneradorDominios.innerHTML = generadorDominios[IndiceDominios]
+        IndiceDominios += 1
+    }
+    boton.addEventListener('click', salidaOrdenadaDominios)
+
+}
